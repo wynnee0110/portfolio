95a45929b3dd4c8f279a854ff15e3e83c24fb6c8
wynnee0110 <wynnee0110@gmail.com>
2025-07-21 11:12:28 +0800

    Projects

---
+++ slider.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const allSlides = document.querySelectorAll('.projects-slider');
+
+  allSlides.forEach(slider => {
+    const slidesContainer = slider.querySelector('.slides');
+    const images = slidesContainer.querySelectorAll('img');
+    let index = 0;
+
+    setInterval(() => {
+      index++;
+      if (index >= images.length) index = 0;
+      slidesContainer.scrollTo({
+        left: images[index].offsetLeft,
+        behavior: 'smooth'
+      });
+    }, 3000); // Slide every 3 seconds
+  });
+});
